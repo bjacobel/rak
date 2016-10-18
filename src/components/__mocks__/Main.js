8e50c9f835d0ea9c8e0a4833a26ee9288abe9da0
Brian Jacobel <bjacobel@gmail.com>
2016-10-18 14:03:04 -0400

    jest has irritating limitations with mocks (see https://github.com/facebook/jest/issues/1305)

---
+++ src/components/__mocks__/Main.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/no-multi-comp */
+import React, { Component } from 'react';
+
+class MainComponent extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default class Main extends Component {
+  render() {
+    return <MainComponent />;
+  }
+}
--- tests/components/routes-test.js
+++ tests/components/routes-test.js
@@ -6,7 +6,7 @@ import Routes from '../../src/components/Routes';
 import React from 'react';
 import { mount } from 'enzyme';
 
-const setHref = (value) => {
+const setPath = (value) => {
   // thanks @cpojer! https://github.com/facebook/jest/issues/890#issuecomment-209698782
   Object.defineProperty(window.location, 'pathname', {
     writable: true,
@@ -16,20 +16,20 @@ const setHref = (value) => {
 
 describe('Routes component', () => {
   it('has a home route', () => {
-    setHref('/');
+    setPath('/');
     const routes = mount(<Routes />);
-    expect(routes.find('Connect(MainComponent)').length).toBe(1);
+    expect(routes.find('MainComponent').length).toBe(1);
   });
 
   it('has a route for child views that takes a url param', () => {
-    setHref('/child/1');
+    setPath('/child/1');
     const routes = mount(<Routes />);
     expect(routes.find('Child').length).toBe(1);
     expect(routes.find('Child').props()).toEqual(jasmine.objectContaining({ params: { id: '1' } }));
   });
 
   it('has a fallthrough 404', () => {
-    setHref('/asdfasaddf');
+    setPath('/asdfasaddf');
     const routes = mount(<Routes />);
     expect(routes.find('NotFound').length).toBe(1);
   });
