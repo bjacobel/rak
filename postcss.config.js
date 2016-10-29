6a4828db23e966086d92f2ea53b35c37ae1f153a
Brian Jacobel <bjacobel@gmail.com>
2016-10-28 23:31:05 -0400

    Fix google fonts issue when sourceMaps were on

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const stylelint = require('stylelint');
-const fontMagician = require('postcss-font-magician');
+const fontMagician = require('postcss-font-magician')({
+  // this is required due to a weird bug where if we let PFM use the `//` protocol Webpack style-loader
+  // thinks it's a relative URL and won't load the font when sourceMaps are also enabled
+  protocol: 'https:',
+});
 
 module.exports = {
   plugins: [
