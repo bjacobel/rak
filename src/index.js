4e1d0cc9bd05db3370c0b804da97af67207a303f
Brian Jacobel <bjacobel@gmail.com>
2016-11-22 10:15:15 -0500

    This has been broken in every rak app I've made...

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducer from './reducers';
-import { SHOW_DEVTOOLS } from './constants';
+import { SHOW_DEV_TOOLS } from './constants';
 
-const composeEnhancers = (!SHOW_DEVTOOLS && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;  // eslint-disable-line max-len, no-underscore-dangle
+const composeEnhancers = (SHOW_DEV_TOOLS && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;  // eslint-disable-line max-len, no-underscore-dangle
 
 const store = createStore(reducer, {}, composeEnhancers(
   applyMiddleware(...[thunk]),
