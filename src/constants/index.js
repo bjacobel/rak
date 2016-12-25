e87ff2d4ad441af4e6cf8b7a0239db4369053332
Brian Jacobel <bjacobel@gmail.com>
2016-12-24 18:50:34 -0600

    been doing prod-only vars wrong for a long time

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const SHOW_DEV_TOOLS = !process.env.production;
+export const SHOW_DEV_TOOLS = process.env.NODE_ENV !== 'production';
 export const DATA = { text: 'async data fetched through Redux' };
