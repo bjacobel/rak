365e7919f622620f9fe295254a60ca9ca6b56d00
Brian Jacobel <bjacobel@gmail.com>
2017-01-15 14:05:46 -0500

    Add object rest spread capability via babel plugin

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -3,7 +3,7 @@ import { GET_DATA_SUCCEEDED } from '../actions/data';
 export default (state = {}, action) => {
   switch (action.type) {
   case GET_DATA_SUCCEEDED:
-    return Object.assign({}, state, action.payload.data);
+    return { ...state, ...action.payload.data };
   default:
     return state;
   }
