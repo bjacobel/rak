74903cf45e5284837ac2ca6c5b8bed5a69c26a0e
Brian Jacobel <bjacobel@gmail.com>
2018-04-28 14:12:15 -0400

    update polyfills and shrink build

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import 'babel-polyfill';
+/* eslint-disable import/first */
+import './polyfills';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+/* eslint-enable import/first */
 
 import reducer from './reducers';
 import Routes from './components/Routes';
 import { SHOW_DEV_TOOLS } from './constants';
 
-const composeEnhancers = (SHOW_DEV_TOOLS && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; // eslint-disable-line max-len, no-underscore-dangle
+const composeEnhancers = (SHOW_DEV_TOOLS && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; // eslint-disable-line no-underscore-dangle
 
 const store = createStore(reducer, {}, composeEnhancers(applyMiddleware(...[thunk])));
 
