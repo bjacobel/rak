149c4de503147e510a30d82d1a7b17bb2b70927a
Brian Jacobel <bjacobel@gmail.com>
2019-10-28 10:29:53 -0400

    Fix build issues. Stop supporting npm-cli

--- install-test.sh
+++ install-test.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-command -v yalc >/dev/null 2>&1 || yarn global add yalc || npm install -g yalc
-yalc publish
+npx yalc publish
 
 mkdir -p my-new-project
 pushd my-new-project
 
 yarn init -y
-yalc add rak
+npx yalc add rak
 node node_modules/rak/install.js
 yarn run test
+
+diff yarn.lock ../yarn.lock
 
 popd
 rm -rf my-new-project
