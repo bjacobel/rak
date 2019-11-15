c3110ded017a61327dbd43f50135167a91dba0e4
Brian Jacobel <bjacobel@gmail.com>
2019-11-14 23:58:50 -0500

    figure out ejs includes when I can

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,32 @@
-module.exports = api => ({
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'entry',
-        modules: api.env('test') ? 'commonjs' : false,
-        exclude: ['transform-regenerator'],
-      },
-    ],
-    '@babel/preset-react',
-  ],
-  plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
-    api.env('development') && '@babel/plugin-transform-react-jsx-source',
-  ].filter(x => x),
-});
+const merge = require('babel-merge');
+
+module.exports = api => {
+  const base = {
+    presets: ['@babel/preset-react'],
+    plugins: [
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-transform-runtime',
+      api.env('development') && '@babel/plugin-transform-react-jsx-source',
+    ].filter(Boolean),
+  };
+
+  return {
+    env: {
+      modern: merge(base, {
+        presets: [['@babel/preset-modules', { loose: true }]],
+      }),
+      legacy: merge(base, {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              useBuiltIns: 'entry',
+              modules: api.env('test') ? 'commonjs' : false,
+              exclude: ['transform-regenerator'],
+            },
+          ],
+        ],
+      }),
+    },
+  };
+};
