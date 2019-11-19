5a8dac8c2a6a6610f2138b5531901c0fee9ce5d6
Brian Jacobel <bjacobel@gmail.com>
2019-11-19 11:32:36 -0500

    Too much fucking work to save like two kb

--- babel.config.js
+++ babel.config.js
@@ -1,33 +1,19 @@
-const merge = require('babel-merge');
-
-module.exports = api => {
-  const base = {
-    presets: ['@babel/preset-react'],
-    plugins: [
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-transform-runtime',
-      api.env('development') && '@babel/plugin-transform-react-jsx-source',
-    ].filter(Boolean),
-  };
-
-  return {
-    env: {
-      modern: merge(base, {
-        presets: [['@babel/preset-modules', { loose: true }]],
-      }),
-      legacy: merge(base, {
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              useBuiltIns: 'entry',
-              corejs: 3,
-              modules: api.env('test') ? 'commonjs' : false,
-              exclude: ['transform-regenerator'],
-            },
-          ],
-        ],
-      }),
-    },
-  };
-};
+module.exports = api => ({
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+        corejs: 3,
+        modules: api.env('test') ? 'commonjs' : false,
+        exclude: ['transform-regenerator'],
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
+    api.env('development') && '@babel/plugin-transform-react-jsx-source',
+  ].filter(Boolean),
+});
