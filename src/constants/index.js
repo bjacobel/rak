be12cbd8db920660df8f05c4fab411b5a655a904
Brian Jacobel <bjacobel@gmail.com>
2019-11-21 13:53:22 -0500

    fix issues caused by defineplugin changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 // injected by DefinePlugin
-/* globals rakConfig */
+/* globals projectConfig */
 
 export const SHOW_DEV_TOOLS = process.env.NODE_ENV !== 'production';
 export const TRACK_ANALYTICS = process.env.NODE_ENV === 'production';
@@ -7,6 +7,6 @@ export const LOG_ERRORS = process.env.NODE_ENV === 'production';
 export const DATA = {
   text: 'an opinionated skeleton to quickly set up a new React app',
 };
-export const RAVEN_ENDPT = rakConfig.RavenDSN;
-export const GA_ID = rakConfig.GAProperty;
+export const RAVEN_ENDPT = projectConfig.RavenDSN;
+export const GA_ID = projectConfig.GAProperty;
 export const RELEASE = process.env.TRAVIS_COMMIT;
