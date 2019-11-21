3f18d7f16b0d2fab7f9cd78a35fd67b3e2e0f157
Brian Jacobel <bjacobel@gmail.com>
2019-11-21 11:23:30 -0500

    Make sure sentry catches normal errors, not just redux ones

--- src/index.js
+++ src/index.js
@@ -10,11 +10,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducer from './reducers';
 import Routes from './components/Routes';
+import { setup as sentrySetup } from './services/errors';
 import { SHOW_DEV_TOOLS } from './constants';
 
 const composeEnhancers = (SHOW_DEV_TOOLS && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; // eslint-disable-line no-underscore-dangle
 
 const store = createStore(reducer, {}, composeEnhancers(applyMiddleware(...[thunk])));
+
+sentrySetup();
 
 const rootEl = document.getElementById('main');
 const render = () => {
