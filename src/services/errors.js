879d26360957bde17d3988f3781a0b9acc8b9f73
Brian Jacobel <bjacobel@gmail.com>
2019-11-21 11:23:30 -0500

    Make sure sentry catches normal errors, not just redux ones

--- src/services/errors.js
+++ src/services/errors.js
@@ -2,9 +2,20 @@ import { init, captureException, configureScope } from '@sentry/browser';
 
 import { LOG_ERRORS, RAVEN_ENDPT, RELEASE } from '../constants';
 
+export const setup = () => {
+  if (LOG_ERRORS) {
+    /* eslint-disable no-underscore-dangle */
+    if (!window.__SENTRY_READY__) {
+      init({ dsn: RAVEN_ENDPT });
+      window.__SENTRY_READY__ = true;
+    }
+    /* eslint-enable no-underscore-dangle */
+  }
+};
+
 export default (ex, context) => {
   if (LOG_ERRORS) {
-    init({ dsn: RAVEN_ENDPT });
+    setup(); // memoized, it is fine to call this on every log
 
     configureScope(scope => scope.setExtra('release', RELEASE));
 
