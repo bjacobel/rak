6720f55ae756323e7199d4498282ce76e675cd24
Brian Jacobel <bjacobel@gmail.com>
2021-11-27 21:48:17 -0500

    CSS is now finally working

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import styles from '../stylesheets/notFound.css';
+import { notFound } from '../stylesheets/notFound.css';
 import { link } from '../stylesheets/link.css';
 
 export default class NotFound extends Component {
   render() {
     return (
       <div>
-        <h1 className={styles['not-found']}>404: page not found</h1>
+        <h1 className={notFound}>404: page not found</h1>
         <Link className={link} to="/">
           Home
         </Link>
