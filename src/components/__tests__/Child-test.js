8aec2ed2896d2e435d707238c4e35b1f722790e7
Brian Jacobel <bjacobel@gmail.com>
2022-10-12 22:16:02 -0400

    Christ this is so much better

--- src/components/__tests__/Child-test.js
+++ src/components/__tests__/Child-test.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { useParams } from '@reach/router';
 
 import Child from 'components/Child';
 
+jest.mock('@reach/router');
+
 describe('child component', () => {
   it('matches snapshot', () => {
-    expect(shallow(<Child match={{ params: { id: 'foo' } }} />)).toMatchSnapshot();
+    useParams.mockReturnValueOnce({ id: 'foo' });
+    expect(shallow(<Child />)).toMatchSnapshot();
   });
 
   it('displays the param it recieves in props', () => {
-    const child = shallow(<Child match={{ params: { id: 'foo' } }} />);
+    useParams.mockReturnValueOnce({ id: 'foo' });
+    const child = shallow(<Child />);
 
     expect(child.find('h3').length).toBe(1);
     expect(child.find('h3').text()).toEqual('received param: foo');
   });
 
   it('has a link back to home', () => {
-    const child = shallow(<Child match={{ params: { id: 'foo' } }} />);
+    useParams.mockReturnValueOnce({ id: 'foo' });
+    const child = shallow(<Child />);
     const homeLink = child.find('Link');
 
     expect(homeLink.length).toBe(1);
