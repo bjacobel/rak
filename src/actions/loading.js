c4be32dfacc4950307c5015c1f95b53b210d1fad
Brian Jacobel <bjacobel@gmail.com>
2022-10-13 21:05:49 -0400

    Remove redux in favor of react-query

--- src/actions/loading.js
+++
@@ -1,6 +0,0 @@
-export const LOADING_STARTED = 'LOADING_STARTED';
-export const LOADING_ENDED = 'LOADING_ENDED';
-
-export const loadingStarted = () => ({ type: LOADING_STARTED });
-
-export const loadingEnded = () => ({ type: LOADING_ENDED });
--- src/index.js
+++ src/index.js
@@ -2,29 +2,22 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
-import reducer from './reducers';
 import Analytics from './components/Analytics';
 import Routes from './components/Routes';
 import { setup as sentrySetup } from './services/errors';
-import { SHOW_DEV_TOOLS } from './constants';
-
-const composeEnhancers = (SHOW_DEV_TOOLS && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; // eslint-disable-line no-underscore-dangle
-
-const store = createStore(reducer, {}, composeEnhancers(applyMiddleware(...[thunk])));
 
 sentrySetup();
+const queryClient = new QueryClient();
 
 const rootEl = document.getElementById('main');
 const render = () => {
   ReactDOM.render(
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
       <Analytics />
       <Routes />
-    </Provider>,
+    </QueryClientProvider>,
     rootEl,
   );
 };
