955a651a844b2ea37d93722ebea134ea9da6a815
Brian Jacobel <bjacobel@gmail.com>
2022-10-16 20:33:11 -0400

    Migrate back to react-router sigh

--- src/components/__tests__/Child-test.js
+++ src/components/__tests__/Child-test.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Child from 'components/Child';
 import { render } from 'testing/rtl';
 
 describe('child component', () => {
+  let child;
+
+  beforeEach(() => {
+    child = (
+      <Routes>
+        <Route element={<Child />} path="/child/:id" />;
+      </Routes>
+    );
+  });
+
   it('matches snapshot', () => {
-    const { asFragment } = render(<Child path="/child/:id" />, { route: '/child/foo' });
+    const { asFragment } = render(child, { route: '/child/foo' });
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('displays the param it recieves in props', async () => {
-    const { getByText } = render(<Child path="/child/:id" />, { route: '/child/foo' });
+    const { getByText } = render(child, { route: '/child/foo' });
     expect(getByText('received param: foo')).toBeInTheDocument();
   });
 
   it('has a link back to home', async () => {
-    const { getByRole } = render(<Child path="/child/:id" />, { route: '/child/foo' });
+    const { getByRole } = render(child, { route: '/child/foo' });
     const homeLink = getByRole('link');
     expect(new URL(homeLink.href).pathname).toEqual('/');
   });
