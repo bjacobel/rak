46cf09e880c0b094a56afd500f7c9a7e19933dd0
Brian Jacobel <bjacobel@gmail.com>
2022-10-23 20:55:01 -0400

    Use babel-loader for typescript

--- src/index.html.tsx
+++ src/index.html.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default (
   <html lang="en">
     <head>
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="preconnect" href="https://www.google-analytics.com" />
       <title>{htmlWebpackPlugin.options.title}</title>
