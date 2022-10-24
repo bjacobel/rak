76dd2c6b0daf947ed2d038daee2262ffb01bdbb8
Brian Jacobel <bjacobel@gmail.com>
2022-10-23 23:02:28 -0400

    no longer needed

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
   coveragePathIgnorePatterns: ['polyfills.js', 'testing/.*.js'],
-  moduleNameMapper: {
-    '^.+\\.css$': '<rootDir>/__mocks__/stylesheets.js',
-  },
   moduleDirectories: [__dirname, 'node_modules', 'src'],
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
