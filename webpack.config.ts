d21f52c6ba29d6a478124c16c87deae9149a5403
Brian Jacobel <bjacobel@gmail.com>
2023-05-06 19:42:12 -0400

    sw-precache plugin is not maintained

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,6 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import TerserPlugin from 'terser-webpack-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import WebpackManifestPlugin from 'webpack-manifest-plugin';
-import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
 
 import config from './config';
 
@@ -119,25 +118,6 @@ export default (env: Record<string, unknown> = {}, { mode }: { mode?: string } =
         chunkFilename: isProd ? '[id].[contenthash].css' : '[id].css',
       }),
       new WebpackManifestPlugin(),
-      new SWPrecacheWebpackPlugin({
-        cacheId: config.ProjectName,
-        // By default, a cache-busting query parameter is appended to requests
-        // used to populate the caches, to ensure the responses are fresh.
-        // If a URL is already hashed by Webpack, then there is no concern
-        // about it being stale, and the cache-busting can be skipped.
-        dontCacheBustUrlsMatching: /\.\w{8}\./,
-        filename: 'service-worker.js',
-        logger(message: string) {
-          if (message.indexOf('Total precache size is') === 0) {
-            // This message occurs for every build and is a bit too noisy.
-            return;
-          }
-          console.log(message);
-        },
-        minify: true, // minify and uglify the script
-        navigateFallback: '/index.html',
-        staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-      }),
     ].filter(Boolean),
     devServer: {
       hot: !isProd,
