57bbdfc9a34afb204b47629ef6fbee2a8f7afa07
Brian Jacobel <bjacobel@gmail.com>
2025-09-07 18:22:46 -0400

    Something broke in a minor version of Node??

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -2,15 +2,17 @@ import path from 'path';
 
 const esmDependencies = ['wouter', '@micro-sentry/core', '@micro-sentry/breadcrumbs-plugin'];
 
+const dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export default {
   collectCoverageFrom: ['src/**/*.{js,ts,jsx}', '!src/**/*.css.ts'],
   coveragePathIgnorePatterns: ['polyfills.js', 'testing/.*.js'],
-  moduleDirectories: [__dirname, 'node_modules', 'src'],
+  moduleDirectories: [dirname, 'node_modules', 'src'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
     url: 'http://localhost',
   },
   transformIgnorePatterns: [`/node_modules/(?!(${esmDependencies.join('|')}))`],
-  transform: { '\\.[jt]sx?$': ['babel-jest', { configFile: path.join(__dirname, '.babelrc.js'), rootMode: 'upward' }] },
+  transform: { '\\.[jt]sx?$': ['babel-jest', { configFile: path.join(dirname, '.babelrc.js'), rootMode: 'upward' }] },
 };
